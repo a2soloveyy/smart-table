5ff3d53d5bc2483cc9f49db69be6f35fb57cddbf
Alina Solovey <a2soloveyy@yandex.ru>
2025-10-10 23:42:50 +0700

    Проектная работа 6.2

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -1,14 +1,38 @@
 import {createComparison, defaultRules} from "../lib/compare.js";
 
 // @todo: #4.3 — настроить компаратор
+const compare = createComparison(defaultRules);
+
 
 export function initFiltering(elements, indexes) {
     // @todo: #4.1 — заполнить выпадающие списки опциями
+    Object.keys(indexes)
+  .forEach((elementName) => {
+    elements[elementName].append(
+      ...Object.values(indexes[elementName]).map(name => {
+        const option = document.createElement('option');
+        option.value = name;
+        option.textContent = name;
+        return option;
+      })
+    );
+  });
+
 
     return (data, state, action) => {
         // @todo: #4.2 — обработать очистку поля
+        if (action && action.name === 'clear') {
+  const field = action.dataset.field; // что чистим (date|customer и т.п.)
+  // найдём input рядом с кнопкой
+  const wrapper = action.closest('.filter-wrapper');
+  const input = wrapper ? wrapper.querySelector('input') : null;
+
+  if (input) input.value = '';
+  if (state && field in state) state[field] = '';
+}
+
 
         // @todo: #4.5 — отфильтровать данные используя компаратор
-        return data;
+        return data.filter(row => compare(row, state));
     }
 }
--- src/components/sorting.js
+++ src/components/sorting.js
@@ -1,18 +1,47 @@
-import {sortCollection, sortMap} from "../lib/sort.js";
+import { sortCollection } from "../lib/sort.js";
+const rotateSort = (v) => (v === 'none' ? 'up' : v === 'up' ? 'down' : 'none');
+
+
+
 
 export function initSorting(columns) {
     return (data, state, action) => {
         let field = null;
         let order = null;
 
-        if (action && action.name === 'sort') {
+       if (action && action.name === 'sort') {
+    // вращаем состояние кнопки по карте переходов
+    action.dataset.value = rotateSort[action.dataset.value];
+
+    // сохраняем текущее поле и направление
+    field = action.dataset.field;
+    order = action.dataset.value;
+
+
             // @todo: #3.1 — запомнить выбранный режим сортировки
+            
 
             // @todo: #3.2 — сбросить сортировки остальных колонок
         } else {
+            columns.forEach(column => {
+    if (action && column.dataset.field !== action.dataset.field) {
+        column.dataset.value = 'none';
+    }
+});
+
+
             // @todo: #3.3 — получить выбранный режим сортировки
+            columns.forEach(column => {
+    if (column.dataset.value !== 'none') {
+        field = column.dataset.field;
+        order = column.dataset.value;
+    }
+});
+
         }
 
-        return sortCollection(data, field, order);
+        if (!field || order === 'none') return data;     // без активной сортировки — без изменений
+return sortCollection([...data], field, order);  // с активной — новая коллекция
+
     }
 }
