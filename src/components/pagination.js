18bb94c77b648fc36ca87f256e27a9e81b15ac8a
Alina Solovey <a2soloveyy@yandex.ru>
2026-02-15 21:42:17 +0700

    Refactor pagination functions and variable declarations
    Refactor pagination logic to improve readability and maintainability.

--- src/components/pagination.js
+++ src/components/pagination.js
@@ -3,15 +3,13 @@ import { getPages } from "../lib/utils.js";
 export const initPagination = (elements, createPage) => {
   const { pages, fromRow, toRow, totalRows } = elements;
 
-  let pageCount;           // количество страниц после ответа сервера
-  let pageTemplate = null; // шаблон кнопки страницы
+  let pageCount;
+  let pageTemplate = null
 
-  // ДО запроса: формируем query
   const applyPagination = (query, state, action) => {
     const limit = state.rowsPerPage;
     let page = state.page;
 
-    // обработка действий пагинации
     if (action) switch (action.name) {
       case 'prev':  page = Math.max(1, page - 1); break;
       case 'next':  page = pageCount ? Math.min(pageCount, page + 1) : page + 1; break;
@@ -22,17 +20,15 @@ export const initPagination = (elements, createPage) => {
     return Object.assign({}, query, { limit, page });
   };
 
-  // ПОСЛЕ запроса: перерисовать кнопки и статус
   const updatePagination = (total, { page, limit }) => {
     pageCount = Math.ceil(total / limit);
 
-    // шаблон кнопки страницы
     if (!pageTemplate) {
       pageTemplate = pages.firstElementChild.cloneNode(true);
       pages.firstElementChild.remove();
     }
 
-    // номера страниц
+
     const visiblePages = getPages(page, pageCount, 5);
     pages.replaceChildren(
       ...visiblePages.map(pageNumber => {
@@ -41,7 +37,6 @@ export const initPagination = (elements, createPage) => {
       })
     );
 
-    // статус
     const from = total ? (page - 1) * limit + 1 : 0;
     fromRow.textContent = from;
     toRow.textContent = Math.min(page * limit, total);
